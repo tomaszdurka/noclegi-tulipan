a0e5235ff2ea72656896a5444341ff89c841e258
Tomasz Durka <tomasz@durka.pl>
2013-06-17 02:12:53 +0200

    Add deploy script

--- deploy/debian.sh
+++ deploy/debian.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-apt-get install apache2 php5 libapache2-mod-php5
+apt-get install apache2
 
 cd $(dirname $0)
+cp vhost.conf /etc/apache2/sites-available/pensjonat-tulipan.pl
+
+cd ..
+mkdir -p /var/www
 ln -s $(pwd) /var/www/pensjonat-tulipan.pl
 
+a2ensite pensjonat-tulipan.pl
+/etc/init.d/apache2 reload
