2255f1e3e8ac947a8919c090332a79fd0fceb7ef
tomaszdurka <tomasz@durka.pl>
2014-10-02 22:16:50 +0200

    Use php

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pl">
+	<head>
+		<link href="/css/all.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="/css/plugins.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="/css/fonts/OpenSans.css" media="screen" rel="stylesheet" type="text/css" />
+
+		<title>Pensjonat Tulipan | Noclegi Konin</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta http-equiv="Content-Language" content="pl" />
+		<meta name="language" content="pl">
+		<meta name="description" content="Oferujemy tanie noclegi, kwatery pracownicze koło Konina. Pokoje jedno, dwu, trzy lub cztero osobowe. Telewizja, internet, WiFi, ogród i kuchnia do wyłącznej dyspozycji gości. Cena noclegu 20 zł za osobę." />
+		<meta name="keywords" content="noclegi, Konin, kwatery, pracownicze, pensjonat, hotel, Brzeńno, autostrada, motel, agroturystyka, ogród, dom wypoczynkowy, wczasy, grill, swoboda, internet, wifi, telewizja" />
+	</head>
+
+	<body>
+		<div class="body-wrapper">
+			<div class="site-header-wrapper">
+				<div class="site-header">
+					<div class="contact-phone">tel. 663 747 326</div>
+					<h1>Pensjonat <span>Tulipan</span></h1>
+					<div class="slogan">
+						Idealne miejsce na wypoczynek. Pensjonat otoczony jest wspaniałymi teranami rekreacyjnymi, będąc jednocześnie tylko 5km od centrum miasta Konin.
+					</div>
+					<div class="clear"></div>
+				</div>
+			</div>
+
+			<div class="site-content-wrapper">
+				<div class="content">
+					<div>
+						<h2>Serdecznie zapraszamy</h2>
+						Zapraszamy do skorzystania z noclegów w cichej i spokojnej miejscowości położonej w odległości 5 km od centrum Konina.
+						Oferujemy pokoje dwu, trzy i wieloosobowe z widokiem na lokalną atrakcję - rezerwat przyrody Złota Góra.
+						We wszystkich pokojach znajdują się telewizory, natomiast w otwartej kuchni mają Państwo możliwość korzystania z kuchenki, piekarnika, mikrofali oraz lodówki.
+					</div>
+					<div class="images">
+						<a class="fancybox" rel="gal" href="/assets/photos/room1.jpg"><img alt="Pokoje Konin" src="/assets/photos/thumb/room1.jpg" /></a>
+						<a class="fancybox" rel="gal" href="/assets/photos/room2.jpg"><img alt="Kwatery pracownicze Konin" src="/assets/photos/thumb/room2.jpg" /></a>
+						<a class="fancybox" rel="gal" href="/assets/photos/garden.jpg"><img alt="Pensjonat Grill Konin" src="/assets/photos/thumb/garden.jpg" /></a>
+					</div>
+					<div>
+						Do swobodnej dyspozycji gości pozostawiamy również miejsca parkingowe w zamkniętych garażach oraz przestronny ogród z miejscem na grilla i ognisko.
+						W całym budynku dostępna sieć wi-fi. W pobliżu sklepy spożywcze oraz restauracje. W odległości 20 m od budynku znajduje się przystanek autobusu miejskiego.
+					</div>
+
+					<br />
+					<div>
+						<div class="google-iframe-wrapper">
+							<iframe class="google-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://mapy.google.pl/maps/ms?msa=0&amp;msid=211353188404189954275.0004b924733d9aecf95ad&amp;ie=UTF8&amp;t=m&amp;ll=52.186984,18.29361&amp;spn=0.126292,0.332336&amp;z=11&amp;output=embed"></iframe>
+							<a class="google-link" href="http://mapy.google.pl/maps/ms?msa=0&amp;msid=211353188404189954275.0004b924733d9aecf95ad&amp;ie=UTF8&amp;t=m&amp;source=embed&amp;z=11">Otwórz większą mapę w nowym oknie</a>
+						</div>
+						<h2>Jak do nas trafić</h2>
+						<div>
+							Brzeźno znajduje się dokładnie między centrum Konina a zjazdem z autostrady A2.
+							Dzięki temu jest łatwo dostępnym, idealnym miejscem na przerwę w czase dłuższej podróży.
+						</div>
+					</div>
+					<div>
+						<h2>Cennik</h2>
+						Koszt noclegu wynosi <b>30 zł </b>od osoby. Dla większych grup atrakcyjne zniżki.
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+
+		<div class="site-footer">
+			<div class="copyrights">
+				<b>Copyright Pensjonat Tulipan</b>,
+				Leśna 22 Brzeźno, Konin
+				tel. 663 747 326
+			</div>
+			<div class="created-by">
+				<a href="http://www.codespot.pl">wykonanie: Codespot</a>
+			</div>
+		</div>
+
+		<script type="text/javascript" src="/js/plugins.js"></script>
+		<script type="text/javascript" src="/js/all.js"></script>
+		<script type="text/javascript" src="">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-26296470-3']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script');
+				ga.type = 'text/javascript';
+				ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+	</body>
+</html>
